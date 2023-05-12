e63938db0d5410a81bd2389eb6866fd0fd7bbef3
Adel Aziz Boulfekhar <boulfekhar.adelaziz@gmail.com>
2023-05-12 22:47:40 +0100

    seperate ssg helper, full post page

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -2,15 +2,13 @@ import { type InferGetStaticPropsType, type NextPage } from "next";
 import Head from "next/head";
 
 import { api } from "~/utils/api";
-import { createServerSideHelpers } from "@trpc/react-query/server";
 import { type GetServerSidePropsContext } from "next";
-import { prisma } from "~/server/db";
-import { appRouter } from "~/server/api/root";
-import superjson from "superjson";
+
 import { PageLayout } from "~/components/layout";
 import Image from "next/image";
 import { LoadingPage } from "~/components/loading";
 import { PostView } from "~/components/postView";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
 type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
 
 const ProfileFeed = (props: { userId: string }) => {
@@ -61,11 +59,7 @@ const ProfilePage: NextPage<PageProps> = ({ username }) => {
 export default ProfilePage;
 
 export const getStaticProps = async (context: GetServerSidePropsContext) => {
-  const ssg = createServerSideHelpers({
-    router: appRouter,
-    ctx: { prisma, userId: null },
-    transformer: superjson,
-  });
+  const ssg = generateSSGHelper();
   const slug = context.params?.slug;
   if (typeof slug !== "string") throw new Error("No slug!");
 
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -82,4 +82,13 @@ export const postsRouter = createTRPCRouter({
         })
         .then(addUserDataToPosts);
     }),
+  getById: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      const post = await ctx.prisma.post.findUnique({
+        where: { id: input.id },
+      });
+      if (!post) throw new TRPCError({ code: "NOT_FOUND" });
+      return (await addUserDataToPosts([post]))[0];
+    }),
 });
